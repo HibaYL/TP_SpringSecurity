56f9b6aed867094fe6ec54f838492895646a2ba2
HibaYL <127992912+HibaYL@users.noreply.github.com>
2024-01-15 22:13:18 +0100

    Add files via upload

---
+++ SpringSecurity-main/SpringSecurity-main/springsecsecurity/src/main/java/com/xproce/entity/Customer.java
@@ -0,0 +1,20 @@
+package com.xproce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String pwd;
+    private String role;
+}
